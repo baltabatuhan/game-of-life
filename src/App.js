3686811f0524c75070a2f8f53ed7764bf903a004
Batuhan Balta <bthn.balta@gmail.com>
2020-10-21 19:11:30 -0700

    features added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Grid } from "./game";
+import GridTable from "./GridTable";
 
 function App() {
   const [game, setGame] = useState({});
@@ -8,6 +9,11 @@ function App() {
   const [col, setCol] = useState(0);
   const [playing, setPlaying] = useState(false);
   const [year, setYear] = useState(0);
+  const [end, setEnd] = useState("");
+  const [ms, setMs] = useState(500);
+  const [isStable, setIsStable] = useState(false);
+
+  console.log({ grid });
 
   const handleCreateGrid = (e) => {
     e.preventDefault();
@@ -15,17 +21,33 @@ function App() {
     setGame(game);
     setGrid(game.current_life());
   };
+  const handleReset = () => {
+    game.reset();
+    let new_grid = game.current_life();
+    setGrid(new_grid);
+  };
 
   const toggleCellLife = (row, col) => {
     game.toggle_life(row, col);
     setGrid(game?.current_life());
   };
-  const toggleStart = () => setPlaying(!playing);
+  const toggleStart = () => {
+    if (!playing && game?.total_cell_count() === 0) {
+      alert("Revive some cells to start the life on the planet");
+    }
+    if (!playing) {
+      setEnd("");
+      setYear(0);
+      setIsStable(false);
+    }
+    setPlaying(!playing);
+  };
 
   useEffect(() => {
     var interval;
     if (playing) {
       interval = setInterval(() => {
+        let old_grid = game.current_life();
         let new_grid = game.next_life();
         setGrid(new_grid);
         setYear((y) => y + 1);
@@ -39,32 +61,52 @@ function App() {
   }, [playing]);
 
   return (
-    <form onSubmit={handleCreateGrid}>
-      Rows: <input value={row} onChange={(e) => setRow(e.target.value)}></input>
-      Cols: <input value={col} onChange={(e) => setCol(e.target.value)}></input>
-      <button type="submit">Create Grid</button>
+    <>
+      <form onSubmit={handleCreateGrid}>
+        Rows:{" "}
+        <input
+          disabled={playing}
+          value={row}
+          onChange={(e) => setRow(e.target.value)}
+        ></input>
+        Cols:{" "}
+        <input
+          disabled={playing}
+          value={col}
+          onChange={(e) => setCol(e.target.value)}
+        ></input>
+        <button
+          disabled={playing}
+          type="submit"
+          onClick={() => console.log("clicked")}
+        >
+          Create Grid
+        </button>
+        <button
+          disabled={!Boolean(game) || playing}
+          type="reset"
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+      </form>
       <button onClick={toggleStart}>{playing ? "Stop" : "Start"}</button>
-      <span>{year}</span>
-      <div className="grid" style={{ width: col * 16 }}>
-        {grid.map((rowArray, row) => {
-          return rowArray.map((colValue, col) => {
-            return (
-              <div
-                key={[row, col]}
-                onClick={() => toggleCellLife(row, col)}
-                style={{
-                  width: 15,
-                  height: 15,
-                  borderRight: "1px solid #ccc",
-                  borderBottom: "1px solid #ccc",
-                  backgroundColor: grid[row][col] ? "black" : undefined,
-                }}
-              ></div>
-            );
-          });
-        })}
-      </div>
-    </form>
+      Set Speed ({ms}ms):
+      <input
+        disabled={playing}
+        value={ms}
+        onChange={(e) => setMs(e.target.value)}
+        type="range"
+        min="100"
+        max="1000"
+      />
+      Life:{" "}
+      <span>
+        {year} {end}
+      </span>
+      {<span>{isStable && "Life is stable on the planet now."}</span>}
+      {game && <GridTable {...{ grid, toggleCellLife, col }} />}
+    </>
   );
 }
 
